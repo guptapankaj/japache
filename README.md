8b22aeed3d3515693750b1c20d9ead0dafa4f9f5
Pankaj Gupta <pankaj_gupta@intuit.com>
2016-08-27 17:02:30 +0530

    adding sample test file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # japache
 HTTP server in java
+
+Supports GET from one client at a time from a given directory in conf
---
+++ src/main/java/SimpleHttpServer.java
@@ -0,0 +1,14 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by pgupta10 on 27/08/16.
+ */
+public class SimpleHttpServer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleHttpServer.class);
+
+    public static void main(String[] args) {
+        LOGGER.debug("hello");
+    }
+}
